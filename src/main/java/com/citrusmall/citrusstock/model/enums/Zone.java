040ac396514ef5ed1e355a7c00563e317f28356c
b.vlasevsky <vlasevskyboris@gmailc.om>
2025-02-27 00:26:54 +0400

    -  Удаление enum Zone

--- src/main/java/com/citrusmall/citrusstock/model/enums/Zone.java
+++
@@ -1,7 +0,0 @@
-package com.citrusmall.citrusstock.model.enums;
-
-public enum Zone {
-    RECEIVING, // Receiving zone (default when a box is created)
-    STORAGE,   // Storage zone (after scanning new goods)
-    SHIPMENT   // Shipment zone (when goods are being shipped)
-}
